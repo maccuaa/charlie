242a290f847d28255310ab6efde5f68d56106017
Andrew MacCuaig <andrew.maccuaig@entrustdatacard.com>
2019-09-15 20:50:15 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import { formatDistanceStrict } from "date-fns"
+
+const CHARLIE_BIRTHDAY = new Date(2019, 5, 19);
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>{`Charlie is ${formatDistanceStrict(CHARLIE_BIRTHDAY, new Date())} old`}</h1>
+    <p></p>
+    <h1></h1>
   </Layout>
 )
 
