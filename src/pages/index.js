d89fd1637d9a5b2711b9210b750c8a04f00b6018
Andrew MacCuaig <andrew.maccuaig@entrustdatacard.com>
2019-09-16 15:49:48 -0400

    Fix date parsing. Fix Charlie birthday

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,52 @@
 import React from "react"
-import 'normalize.css';
+import "normalize.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { formatDistanceStrict } from "date-fns"
+import {
+  differenceInYears,
+  differenceInMonths,
+  differenceInDays,
+  subYears,
+  subMonths,
+} from "date-fns"
 
-const CHARLIE_BIRTHDAY = new Date(2019, 5, 19);
+const CHARLIE_BIRTHDAY = new Date("2019 May 19")
+
+function datediff(from, to) {
+  const result = []
+
+  const years = differenceInYears(to, from)
+  if (years > 0) {
+    result.push(`${years} years`)
+    to = subYears(to, years)
+  }
+
+  const months = differenceInMonths(to, from)
+  if (months > 0) {
+    result.push(`${months} months`)
+    to = subMonths(to, months)
+  }
+
+  const days = differenceInDays(to, from)
+  if (days > 0) {
+    result.push(`${days} days`)
+  }
+
+  return result.join(" ") //1 years 4 months 13 days
+}
 
 const IndexPage = () => {
-  console.log('hello')
+  const TODAY = new Date()
+
+  const diff = datediff(CHARLIE_BIRTHDAY, TODAY)
+
   return (
-  <Layout>
-    <SEO title="Home" />
-    <h1>{`Charlie is ${formatDistanceStrict(CHARLIE_BIRTHDAY, new Date())} old`}</h1>
-  </Layout>
-)}
+    <Layout>
+      <SEO title="Home" />
+      <h1>{`Charlie is ${diff}`}</h1>
+    </Layout>
+  )
+}
 
 export default IndexPage
