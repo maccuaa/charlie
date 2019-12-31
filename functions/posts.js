af185f11bad4dc4a19d571feeaf598dc087d75fc
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2019-12-31 01:22:15 -0500

    add logging to function

--- functions/posts.js
+++ functions/posts.js
@@ -1,18 +1,24 @@
 const instagramPosts = require("instagram-posts");
 
-exports.handler = async (event, context) => {
+exports.handler = event => {
   if (event.httpMethod !== "GET") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
   const promise = new Promise(async (resolve, reject) => {
     try {
-      const posts = await instagramPosts("the.charlie.dood", { count: 10 });
+      console.log("Fetching posts...");
+
+      const posts = await instagramPosts("the.charlie.dood");
+
+      console.log("Successfully got posts");
+
       resolve({
         statusCode: 200,
         body: JSON.stringify(posts)
       });
     } catch (e) {
+      console.log("Failed to get posts", e);
       reject({
         statusCode: 500,
         body: e
