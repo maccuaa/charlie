91616bf98bd4a9d234508e0dce7587fb20a1bd0f
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2020-01-25 14:40:19 -0500

    bump deps

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,61 +1,59 @@
 import React from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div id="root">
-      <style global jsx>{`
-        @import url("https://fonts.googleapis.com/css?family=Quicksand:300,&display=swap");
+const Layout = ({ children }: { children: React.ReactNode }) => (
+  <div id="root">
+    <style global jsx>{`
+      @import url("https://fonts.googleapis.com/css?family=Quicksand:300,&display=swap");
 
-        body {
-          font-family: "Quicksand", sans-serif;
-          font-weight: 300;
-          background-color: #9a382a;
-          color: #fff;
-          height: 100vh;
-          margin: 0;
-          padding: 0;
-          width: 100%;
-        }
+      body {
+        font-family: "Quicksand", sans-serif;
+        font-weight: 300;
+        background-color: #9a382a;
+        color: #fff;
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+        width: 100%;
+      }
 
-        #root {
-          text-align: center;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          position: relative;
-          justify-content: center;
+      #root {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        position: relative;
+        justify-content: center;
+      }
+
+      main {
+        margin: 24px;
+      }
+
+      a {
+        color: inherit;
+      }
+
+      @media screen and (min-width: 768px) {
+        #wrapper {
+          margin: 0 auto;
+          max-width: 960px;
         }
 
         main {
-          margin: 24px;
+          margin: 0;
         }
+      }
 
-        a {
-          color: inherit;
+      @media screen and (min-height: 634px) {
+        #root {
+          height: 100vh;
         }
-
-        @media screen and (min-width: 768px) {
-          #wrapper {
-            margin: 0 auto;
-            max-width: 960px;
-          }
-
-          main {
-            margin: 0;
-          }
-        }
-
-        @media screen and (min-height: 634px) {
-          #root {
-            height: 100vh;
-          }
-        }
-      `}</style>
-      <div id="wrapper">
-        <main>{children}</main>
-      </div>
+      }
+    `}</style>
+    <div id="wrapper">
+      <main>{children}</main>
     </div>
-  );
-};
+  </div>
+);
 
 export default Layout;
