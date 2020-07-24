900298d5d4a99649303e2920de0f0e6302c0063e
Andrew MacCuaig <andrewmaccuaig@gmail.com>
2020-07-23 21:24:55 -0400

    invert deploy exit code

--- src/lib/check-latest.ts
+++ src/lib/check-latest.ts
@@ -12,11 +12,11 @@ import { exit } from "process";
   console.log("Deployed shortcode", latest.shortcode);
   console.log("Latest shortcode", shortcode);
 
-  // exit code 1 to abort the deploy
+  // exit code 0 to abort the deploy
   if (latest.shortcode === shortcode) {
-    exit(1);
+    exit(0);
   }
 
-  // exit code 0 to proceed with the deploy
-  exit(0);
+  // exit code 1 to proceed with the deploy
+  exit(1);
 })();
